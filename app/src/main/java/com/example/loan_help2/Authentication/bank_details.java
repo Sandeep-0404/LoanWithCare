d8107ab8b99b2e6c554bce91777ca7178a8c2eaf
Saurav Kumar <https://github.com/sauravkumar-2002/Health-Companion>
2022-07-26 14:08:13 +0530

    sign_up activity created

---
+++ app/src/main/java/com/example/loan_help2/Authentication/bank_details.java
@@ -0,0 +1,16 @@
+package com.example.loan_help2.Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.loan_help2.R;
+
+public class bank_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bank_details);
+    }
+}
