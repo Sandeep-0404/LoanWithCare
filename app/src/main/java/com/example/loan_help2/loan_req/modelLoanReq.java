063d9e41d900de94514333ad36ee43c95c1bfe05
Sandeep-0404 <sandeepgh2002@gmail.com>
2022-09-21 17:21:10 +0530

    singlerow and accept request with cibil score done

---
+++ app/src/main/java/com/example/loan_help2/loan_req/modelLoanReq.java
@@ -0,0 +1,44 @@
+package com.example.loan_help2.loan_req;
+public class modelLoanReq {
+    String username,tenure,rate,amount,cibil;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTenure() {
+        return tenure;
+    }
+
+    public void setTenure(String tenure) {
+        this.tenure = tenure;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getCibil() {
+        return cibil;
+    }
+
+    public void setCibil(String cibil) {
+        this.cibil = cibil;
+    }
+}
